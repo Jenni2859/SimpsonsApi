feb64db121c62a51467c1e391c1c07621dc79a31
Jenni2859 <jenifer.garcia.2859@gmail.com>
2025-04-08 10:00:38 -0600

    Cambio en el drawer

--- src/navigation/DrawerNavigation.tsx
+++ src/navigation/DrawerNavigation.tsx
@@ -1,16 +1,49 @@
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import React from 'react'
 import { View } from 'react-native'
+import HomeScreen from '../screens/HomeScreen';
+import InitialScreen from '../screens/InitialScreen';
 
+
+const Drawer = createDrawerNavigator();
 const DrawerNavigation = () => {
   return (
-    // <Drawer.Navigator>
-    //     <Drawer.Screen name='Inicio' component={HomeScreen}/>
-    //     <Drawer.Screen name='Detalles' component={DetailsScreen}/>
-    // </Drawer.Navigator>
-    <View>
+    <Drawer.Navigator initialRouteName='Inicio'
+    screenOptions={{
+      headerStyle:{
+        backgroundColor:"#fada00",
+      },
+      headerTintColor:"#fff",
+      drawerStyle:{
+        backgroundColor:"#fada00",
         
-    </View>
+      },
+      headerTitleStyle: {
+        fontSize: 22, // ← Cambia el tamaño aquí
+        fontWeight: 'bold', // opcional
+        textShadowColor: '#000',
+        textShadowOffset: { width: 2, height: 2 },
+        textShadowRadius: 2,
+        padding:10,
+      },  
+      drawerLabelStyle: {
+        fontSize: 20, 
+        fontWeight: 'bold', 
+        //color: '#fff', 
+        textShadowColor: '#000',
+        textShadowOffset: { width: 2, height: 2 },
+        textShadowRadius: 2,
+      },
+      drawerActiveTintColor: '#fff', // Color del texto cuando está activo
+      drawerInactiveTintColor: '#f1d5d5', // Color del texto cuando está inactivo
+      // headerLeft:{
+        
+      // }
+    }}
+  >
+    <Drawer.Screen name='Inicio' component={InitialScreen} />
+    <Drawer.Screen name='Personajes' component={HomeScreen} />
+  </Drawer.Navigator>
   )
 }
 
--- src/navigation/StackNavigation.tsx
+++ src/navigation/StackNavigation.tsx
@@ -5,9 +5,10 @@ import InitialScreen from '../screens/InitialScreen';
 import HomeScreen from '../screens/HomeScreen';
 import DetailsScreen from '../screens/DetailsScreen';
 import LoginScreen from '../screens/LoginScreen';
+import DrawerNavigation from './DrawerNavigation';
 
 const Stack = createStackNavigator();
-const Drawer = createDrawerNavigator();
+
 
 const StackNavigation = () => {
   return (
@@ -29,7 +30,7 @@ const StackNavigation = () => {
         <Stack.Screen name="Login" component={LoginScreen} options={{title:'Iniciar Sesión'}} />
         <Stack.Screen
         name='Home'
-        component={Navigation}
+        component={DrawerNavigation}
 
         options={{headerShown: false}}
         />
@@ -38,43 +39,5 @@ const StackNavigation = () => {
   )
 }
 
-const Navigation = () =>{
-    return(<Drawer.Navigator initialRouteName='Inicio'
-      screenOptions={{
-        headerStyle:{
-          backgroundColor:"#fada00",
-        },
-        headerTintColor:"#fff",
-        drawerStyle:{
-          backgroundColor:"#fada00",
-          
-        },
-        headerTitleStyle: {
-          fontSize: 22, // ← Cambia el tamaño aquí
-          fontWeight: 'bold', // opcional
-          textShadowColor: '#000',
-          textShadowOffset: { width: 2, height: 2 },
-          textShadowRadius: 2,
-          padding:10,
-        },  
-        drawerLabelStyle: {
-          fontSize: 20, 
-          fontWeight: 'bold', 
-          //color: '#fff', 
-          textShadowColor: '#000',
-          textShadowOffset: { width: 2, height: 2 },
-          textShadowRadius: 2,
-        },
-        drawerActiveTintColor: '#fff', // Color del texto cuando está activo
-        drawerInactiveTintColor: '#f1d5d5', // Color del texto cuando está inactivo
-        // headerLeft:{
-          
-        // }
-      }}
-    >
-      <Drawer.Screen name='Inicio' component={InitialScreen} />
-      <Drawer.Screen name='Personajes' component={HomeScreen} />
-    </Drawer.Navigator>)
-  }
 
 export default StackNavigation
